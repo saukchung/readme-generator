acc1e53ce4dc18a1ca9f996ed725dc02ee6a51ec
Sa Uk Chung <chungsauk95@gmail.com>
2022-06-07 22:59:30 -0400

    Forgot to commit the index in the last commit: arrray of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,72 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  //Title
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of this README?",
+  },
+
+  //Description Content
+  {
+    type: "input",
+    name: "description",
+    message: "What would you like to put under the Description section?",
+  },
+
+  //installation instructions
+  {
+    type: "input",
+    name: "installInformation",
+    message: "Installation instructions?",
+  },
+
+  //Usage Information
+  {
+    type: "input",
+    name: "usageInformation",
+    message: "Usage Information?",
+  },
+
+  //Contributions
+  {
+    type: "input",
+    name: "contributionInformation",
+    message: "Contributions?",
+  },
+
+  //Test Instructions
+  {
+    type: "input",
+    name: "testInformation",
+    message: "Test Information?",
+  },
+
+  //Choose a license
+  {
+    type: "list",
+    name: "licenseInformation",
+    message: "Choose a license?",
+  },
+
+  //GitHub Username
+  {
+    type: "input",
+    name: "contactGithub",
+    message: "Github?",
+  },
+
+  //Email
+  {
+    type: "input",
+    name: "contactEmail",
+    message: "Email?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
